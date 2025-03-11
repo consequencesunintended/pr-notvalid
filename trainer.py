21a8378896a2540ff6ae5524062f334b586863fb
consequencesunintended <44167267+consequencesunintended@users.noreply.github.com>
2025-03-11 14:09:52 +0000

    try a second time bruv

--- trainer.py
+++ trainer.py
@@ -109,79 +109,79 @@ class Trainer:
 
         self.model = unet
 
-        for epoch in range(0, 1):
+        for epoch in range(0, 2):
 
           for i, batch in enumerate(data_loader):
 
-            # with self.accelerator.accumulate(self.model):
+            with self.accelerator.accumulate(self.model):
 
 
-            #   with torch.no_grad():
-            #       image = batch["image"].to("cuda")
-            #       depth_image = batch["image_depth"].to("cuda")
+              with torch.no_grad():
+                  image = batch["image"].to("cuda")
+                  depth_image = batch["image_depth"].to("cuda")
 
-            #       x_0 = vae.encode(image).latent_dist.sample()
-            #       x_0 = x_0 * vae.config.scaling_factor
+                  x_0 = vae.encode(image).latent_dist.sample()
+                  x_0 = x_0 * vae.config.scaling_factor
 
-            #       bsz = x_0.shape[0]
+                  bsz = x_0.shape[0]
 
-            #       # Generate a batch of random probabilities, each in [0, 1)
-            #       random_prob = torch.randint(0, 2, (bsz, 1)).float().to("cuda")
-            #       task_emb = torch.cat([random_prob, 1 - random_prob], dim=1).float().to("cuda")
-            #       task_emb = torch.cat([torch.sin(task_emb), torch.cos(task_emb)], dim=1)
+                  # Generate a batch of random probabilities, each in [0, 1)
+                  random_prob = torch.randint(0, 2, (bsz, 1)).float().to("cuda")
+                  task_emb = torch.cat([random_prob, 1 - random_prob], dim=1).float().to("cuda")
+                  task_emb = torch.cat([torch.sin(task_emb), torch.cos(task_emb)], dim=1)
 
-            #       # Sample a random timestep for each image
-            #       fixed_timestep = noise_scheduler.config.num_train_timesteps - 1  # assuming 0-indexing
-            #       timesteps = torch.full((bsz,), fixed_timestep, device=x_0.device, dtype=torch.long)
+                  # Sample a random timestep for each image
+                  fixed_timestep = noise_scheduler.config.num_train_timesteps - 1  # assuming 0-indexing
+                  timesteps = torch.full((bsz,), fixed_timestep, device=x_0.device, dtype=torch.long)
 
-            #       rgb_latents = x_0
+                  rgb_latents = x_0
 
-            #   latent_model_input = rgb_latents
+              latent_model_input = rgb_latents
 
-            #   encoder_hidden_states = text_encoder(batch["input_ids"], return_dict=False)[0]
-            #   noise_pred = self.model(latent_model_input, timesteps, encoder_hidden_states, class_labels=task_emb, return_dict=False)[0]
+              encoder_hidden_states = text_encoder(batch["input_ids"], return_dict=False)[0]
+              noise_pred = self.model(latent_model_input, timesteps, encoder_hidden_states, class_labels=task_emb, return_dict=False)[0]
 
-            #   scalar_timestep = timesteps[0].item()
-            #   noise_scheduler.set_timesteps(noise_scheduler.config.num_train_timesteps, device="cuda")
+              scalar_timestep = timesteps[0].item()
+              noise_scheduler.set_timesteps(noise_scheduler.config.num_train_timesteps, device="cuda")
 
-            #   predicted_annotation_latents = noise_pred
-            #   image_reconstructed_latents = noise_scheduler.step(noise_pred, scalar_timestep, rgb_latents, return_dict=False)[0]
+              predicted_annotation_latents = noise_pred
+              image_reconstructed_latents = noise_scheduler.step(noise_pred, scalar_timestep, rgb_latents, return_dict=False)[0]
 
-            #   predicted_annotation = vae.decode(predicted_annotation_latents / vae.config.scaling_factor, return_dict=False)[0]
+              predicted_annotation = vae.decode(predicted_annotation_latents / vae.config.scaling_factor, return_dict=False)[0]
 
-            #   image_reconstructed = vae.decode(image_reconstructed_latents / vae.config.scaling_factor, return_dict=False)[0]
+              image_reconstructed = vae.decode(image_reconstructed_latents / vae.config.scaling_factor, return_dict=False)[0]
 
-            #   predicted_annotation_loss = F1.mse_loss(predicted_annotation, depth_image, reduction="mean")
-            #   image_reconstructed_loss = F1.mse_loss(image_reconstructed, image, reduction="mean")
+              predicted_annotation_loss = F1.mse_loss(predicted_annotation, depth_image, reduction="mean")
+              image_reconstructed_loss = F1.mse_loss(image_reconstructed, image, reduction="mean")
 
-            #   weights = random_prob.squeeze()
-            #   weighted_loss = weights * predicted_annotation_loss + (1 - weights) * image_reconstructed_loss
-            #   loss = weighted_loss.mean()
+              weights = random_prob.squeeze()
+              weighted_loss = weights * predicted_annotation_loss + (1 - weights) * image_reconstructed_loss
+              loss = weighted_loss.mean()
 
-            #   self.accelerator.backward(loss)
+              self.accelerator.backward(loss)
 
-            #   if self.accelerator.sync_gradients:
-            #     self.accelerator.clip_grad_norm_(self.model.parameters(), 1.0)
-            #   optimizer.step()
-            #   optimizer.zero_grad()
+              if self.accelerator.sync_gradients:
+                self.accelerator.clip_grad_norm_(self.model.parameters(), 1.0)
+              optimizer.step()
+              optimizer.zero_grad()
 
             if self.accelerator.sync_gradients:
 
                 if self.accelerator.is_local_main_process:
                     print(i)
                 
-                # reduced_loss = self.accelerator.reduce(loss, reduction="mean")
+                reduced_loss = self.accelerator.reduce(loss, reduction="mean")
 
-                # if self.accelerator.is_local_main_process:
-                #     print(f'loss:{reduced_loss}', flush=True)
+                if self.accelerator.is_local_main_process:
+                    print(f'loss:{reduced_loss}', flush=True)
 
-                #     predicted_np = (predicted_annotation / 2 + 0.5).clamp(0, 1)
-                #     image_np = predicted_np[0].float().permute(1, 2, 0).detach().cpu().numpy()
-                #     image_np = (image_np * 255).astype(np.uint8)
-                #     im = Image.fromarray(image_np)
-                #     output_dir = "/root/output/images"
-                #     os.makedirs(output_dir, exist_ok=True)
-                #     im.save(f'{output_dir}/my_image_{i}.png')
+                    predicted_np = (predicted_annotation / 2 + 0.5).clamp(0, 1)
+                    image_np = predicted_np[0].float().permute(1, 2, 0).detach().cpu().numpy()
+                    image_np = (image_np * 255).astype(np.uint8)
+                    im = Image.fromarray(image_np)
+                    output_dir = "/root/output/images"
+                    os.makedirs(output_dir, exist_ok=True)
+                    im.save(f'{output_dir}/my_image_{i}.png')
         
         gc.collect()
         torch.cuda.empty_cache()
