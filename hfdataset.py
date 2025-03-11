70fee1d0c994ed84e4488758dd4b18bae3e392a1
consequencesunintended <44167267+consequencesunintended@users.noreply.github.com>
2025-03-11 16:54:32 +0000

    finish him

--- hfdataset.py
+++ hfdataset.py
@@ -63,7 +63,6 @@ class CustomDataset(IterableDataset):
 
 def load_hf_dataset(num_processes, process_index):
     ds_shard = load_dataset("wangherr/coco2017_caption_depth", split="train", streaming=True).shard(num_processes, process_index)
-    shuffled_ds_shard = ds_shard.shuffle(buffer_size=10_000, seed=random.randint(0, 1_000_000))
-    train_dataset = CustomDataset(shuffled_ds_shard.with_format("torch"))
+    train_dataset = CustomDataset(ds_shard.with_format("torch"))
     
     return train_dataset
