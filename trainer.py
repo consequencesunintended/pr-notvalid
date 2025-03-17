924b237992812bb4cf6a1b1795caccc6156a6420
consequencesunintended <44167267+consequencesunintended@users.noreply.github.com>
2025-03-17 01:01:25 +0000

    one more time

--- trainer.py
+++ trainer.py
@@ -106,8 +106,8 @@ class Trainer:
 
         optimizer = torch.optim.AdamW(unet.parameters(), lr=3e-5)
 
-        num_training_steps = 4000
-        num_warmup_steps = 500
+        num_training_steps = 50
+        num_warmup_steps = 10
 
         scheduler = get_cosine_schedule_with_warmup(
             optimizer=optimizer,
